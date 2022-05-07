3c9db59fc4dfdfd4730f62e4f9a717024e917581
bigdiggysquiggle <dylanromansky@yahoo.com>
2022-05-07 16:43:50 -0700

    challenge from 3/7 down

---
+++ March-2022/17/solution.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import re
+
+def getmatch(lst, quer):
+	newlst=[x for x in lst if x.startswith(quer)]
+	return newlst
+
+if __name__ == "__main__":
+	lst=["dog", "deer", "deal"]
+	print(lst)
+	print(getmatch(lst, "de"))
+	lst=["test", "sock", "bean", "string", "cheese", "toast", "shoe", "floor", "stove"]
+	print(lst)
+	print(getmatch(lst, "s"))
